5057b80e65bb7450264aec9f86d6372b83e3ce00
X0X0T <99203039+X0X0T@users.noreply.github.com>
2023-05-23 13:44:58 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="django-insecure-123")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
