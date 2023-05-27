52edd7aac24c23bc1d1dae5640a36b2cf5143a1f
X0X0T <99203039+X0X0T@users.noreply.github.com>
2023-05-27 13:31:46 +0300

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -12,7 +12,7 @@ from .serializers import UserSubscriptionSerializer
 from rest_framework.decorators import action
 
 class CustomUserViewSet(UserViewSet): 
-    queryset = User.objects.all()#.order_by("id") 
+    queryset = User.objects.all().order_by("id") 
     pagination_class = UsersPagination 
     
     @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
